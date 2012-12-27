7e37bd7f6de6e3ef1474b3fc7f531c98e59f6aac
Dominik Frankowski <dominik.frankowski@comarch.com>
2012-12-27 18:25:07 +0100

    fix for bug when opening plain text document

--- java/src/org/ned/client/view/TextDisplayView.java
+++ java/src/org/ned/client/view/TextDisplayView.java
@@ -1,26 +1,27 @@
 /*******************************************************************************
-* Copyright (c) 2011 Nokia Corporation
-* All rights reserved. This program and the accompanying materials
-* are made available under the terms of the Eclipse Public License v1.0
-* which accompanies this distribution, and is available at
-* http://www.eclipse.org/legal/epl-v10.html
-*
-* Contributors:
-* Comarch team - initial API and implementation
-*******************************************************************************/
+ * Copyright (c) 2011-2012 Nokia Corporation
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * Comarch team - initial API and implementation
+ *******************************************************************************/
 package org.ned.client.view;
 
+import com.sun.lwuit.TextArea;
 import com.sun.lwuit.events.ActionEvent;
 import com.sun.lwuit.events.ActionListener;
-import com.sun.lwuit.layouts.BorderLayout;
-import org.ned.client.IContent;
-import org.ned.client.command.BackImageCommand;
 import com.sun.lwuit.html.HTMLComponent;
+import com.sun.lwuit.layouts.BorderLayout;
 import java.io.IOException;
 import java.io.InputStream;
 import javax.microedition.io.Connector;
 import javax.microedition.io.file.FileConnection;
+import org.ned.client.IContent;
 import org.ned.client.NedResources;
+import org.ned.client.command.BackImageCommand;
 import org.ned.client.lwuitExtended.NedRequestHandler;
 
 public class TextDisplayView extends NedFormBase implements ActionListener {
@@ -28,63 +29,70 @@ public class TextDisplayView extends NedFormBase implements ActionListener {
     private HTMLComponent comp = null;
     private IContent mContent;
 
-    public TextDisplayView(IContent content) {
+    public TextDisplayView( IContent content ) {
         mContent = content;
         String fileName = content.getMediaFile();
         setLayout( new BorderLayout() );
         setNedTitle( mContent.getText() );
-        setScrollable(false);
+        setScrollable( false );
 
         FileConnection fc = null;
         InputStream is = null;
         try {
-           fc = (FileConnection)Connector.open(fileName, Connector.READ);
-           if (fc.exists()) {
-               is = fc.openInputStream();
+            fc = (FileConnection)Connector.open( fileName, Connector.READ );
+            if ( fc.exists() ) {
+                is = fc.openInputStream();
                 StringBuffer sb = new StringBuffer();
                 int chars = 0;
-                while ((chars = is.read()) != -1) {
-                    sb.append((char) chars);
+                while ( (chars = is.read()) != -1 ) {
+                    sb.append( (char)chars );
                 }
-               comp = new HTMLComponent( new NedRequestHandler() );
-               addComponent(BorderLayout.CENTER, comp);
-               comp.setShowImages(true);
-               comp.setScrollableX(false);
-               comp.setScrollableY(true);
-               if( fileName.toUpperCase().endsWith("RTF")) {
-                   comp.setBodyText( sb.toString() );
-               } else {
-                   comp.setHTML( sb.toString(), null, mContent.getText(), true );
-               }
+                comp = new HTMLComponent( new NedRequestHandler() );
+                comp.setShowImages( true );
+                comp.setScrollableX( false );
+                comp.setScrollableY( true );
+                if ( fileName.toUpperCase().endsWith( "RTF" ) ) {
+                    comp.setBodyText( sb.toString() );
+                    addComponent( BorderLayout.CENTER, comp );
+                } else if ( fileName.toUpperCase().endsWith( "TXT" ) ) {
+                    TextArea text = new TextArea( sb.toString() );
+                    text.setSelectedStyle( text.getUnselectedStyle() );
+                    text.setGrowByContent( true );
+                    text.setScrollVisible( true );
+                    text.setEditable( false );
+                    addComponent( BorderLayout.CENTER, text );
+                } else {
+                    comp.setHTML( sb.toString(), null, mContent.getText(), true );
+                    addComponent( BorderLayout.CENTER, comp );
+                }
 
             }
-        } catch (IOException ex) {
+        } catch ( IOException ex ) {
             ex.printStackTrace();  // TODO
-        } catch (OutOfMemoryError ex) {
-            GeneralAlert.show(NedResources.MEMORY_OUT, GeneralAlert.WARNING);
+        } catch ( OutOfMemoryError ex ) {
+            GeneralAlert.show( NedResources.MEMORY_OUT, GeneralAlert.WARNING );
         } catch ( Exception ex ) {
             ex.printStackTrace();
-        }
-        finally {
+        } finally {
             try {
                 is.close();
-            } catch (Exception ex) {
+            } catch ( Exception ex ) {
             }
             try {
                 fc.close();
-            } catch (Exception ex) {
+            } catch ( Exception ex ) {
             }
         }
 
-        addCommand(BackImageCommand.getInstance().getCommand());
-        addCommandListener(this);
+        addCommand( BackImageCommand.getInstance().getCommand() );
+        addCommandListener( this );
     }
 
-    public void actionPerformed(ActionEvent evt) {
+    public void actionPerformed( ActionEvent evt ) {
         Object src = evt.getSource();
-        if (src == BackImageCommand.getInstance().getCommand()) {
+        if ( src == BackImageCommand.getInstance().getCommand() ) {
             comp.cancel();
-            BackImageCommand.getInstance().execute(mContent.getParentId());
+            BackImageCommand.getInstance().execute( mContent.getParentId() );
         }
     }
 }
